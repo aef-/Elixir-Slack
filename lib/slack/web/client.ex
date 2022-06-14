2839813887497309faf5f41a02408e3938323b51
aef- <aef@fastmail.com>
2022-06-14 16:21:50 -0400

    update client

--- lib/slack/web/client.ex
+++ lib/slack/web/client.ex
@@ -7,23 +7,23 @@ defmodule Slack.Web.Client do
   Parsed body data is returned unwrapped to the caller.
   """
 
-  use Tesla
-
-  plug(Tesla.Middleware.FormUrlencoded)
-  plug(Tesla.Middleware.JSON, engine: Jason, engine_opts: [keys: :atoms])
-
-  @impl true
   def post!(url, body) do
-    url
-    |> post!(body)
+    Tesla.post!(client(), url, body)
     |> Map.fetch!(:body)
   end
 
-  @impl true
   def post(url, body) do
-    with {:ok, tesla} <- post(url, body),
+    with {:ok, tesla} <- Tesla.post(client(), url, body),
          {:ok, body} <- Map.fetch(tesla, :body) do
       {:ok, body}
     end
+  end
+
+  defp client() do
+    [
+      {Tesla.Middleware.JSON, engine: Jason, engine_opts: [keys: :atoms]},
+      Tesla.Middleware.FormUrlencoded
+    ]
+    |> Tesla.client()
   end
 end
